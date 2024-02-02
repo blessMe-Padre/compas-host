46814e0dcb7e8696ebfce69f7119ecde62002c7c
Сергей Субботин <zarodiny@yandex.ru>
2024-02-02 16:32:55 +1000

    feat: commit

---
+++ template/base.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Template Name: Базовый  - шаблон
+*/
+?>
+<?php get_header(); ?>
+
+<main>
+
+    <section class="page-content py-8 sm:py-16 md:py-32">
+        <div class="container">
+
+            <?php the_content(); ?>
+        </div>
+    </section>
+
+
+
+</main>
+
+<?php get_footer(); ?>
